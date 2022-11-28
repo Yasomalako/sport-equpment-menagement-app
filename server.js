811d767e014fcf8c7dcbd4d74827cdfd5e9ea32f
Yasomalako <Yasomalako@gmail.com>
2022-11-28 14:56:46 +0200

    sportProject

---
+++ server.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv')
+dotenv.config()
+require('./DB')
+const express = require('express')
+const app = express()
+const port = 8989
+const cors = require('cors')
+
+app.use(express.json({express: true}))
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+
+app.listen(port,()=>{
+    console.log(`the server is connected to port ${port}`);
+})
